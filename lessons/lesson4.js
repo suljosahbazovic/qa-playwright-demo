c2bb973cfde1b28022fdb4466732fea5ae1ffc53
Suljo Sahbazovic <suljosahbazovic@gmail.com>
2026-04-08 20:20:04 +0200

    new_changes
    Add new files and delete old two files

---
+++ lessons/lesson4.js
@@ -0,0 +1,15 @@
+//RElational and Equality Operators
+
+// In this lesson, we will explore relational and equality operators in JavaScript.
+// These operators are used to compare values and determine their relationships.
+// Let's start with relational operators: 
+// Greater than (>), Less than (<), Greater than or equal to (>=), and Less than or equal to (<=).
+
+let a = 10;
+let b = 20;
+console.log(a > b);  // false
+console.log(a < b);  // true
+console.log(a >= 10); // true
+console.log(b <= 15); // false
+// Now, let's look at equality operators:
+// Equality (==) and Strict Equality (===)
